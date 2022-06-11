f79535838e936af326ece53123c7ad23edfa5f17
Subash Chandra Bose <b.subi68@gmail.com>
2022-06-11 16:30:06 +0530

    Code refactoring and fix instance for balance tracker

--- src/services/track.service.js
+++ src/services/track.service.js
@@ -1,44 +1,57 @@
 const { Tracker } = require('../models');
+const CronJob = require('cron').CronJob;
 const { BalanceTracker } = require('./balance.tracker.service');
 const { BalanceTrackerBot } = require('./discord.bot.service');
-// call balance tracker
-// call bot service here
+const { handleInput } = require('./cronjob.resolve.service');
 
+let balanceTracker;
 
 const configTracker = async (configBody) => {
-    return Tracker.create(configBody);
+    const config = await getTrackerByName(configBody.tracker)
+    let updatedConfig = (config != null) ? await updateTracker(configBody) : Tracker.create(configBody);
+    await startListening(updatedConfig);
+    return updatedConfig;
 }
 
 const getTrackerByName = async (tracker) => {
-    return Tracker.find({ tracker });
+    return Tracker.findOne({ tracker });
 }
 
-const updateTrakcer = async (configBody) => {
-    const config  = await getTrackerByName(configBody.name);
-    if (config.length == 1) {
-        // update config here
-    }
+const updateTracker = async (configBody) => {
+    const config  = await getTrackerByName(configBody.tracker);
+    Object.assign(config, configBody);
+    await config.save();
+    return config; 
+}
+
+const startListening = async (config) => {
+    balanceTracker = new BalanceTracker(
+        config.addresses,
+        config.minimum,
+        new BalanceTrackerBot()
+    );
+    balanceTracker.subscribe("pendingTransactions");
+    balanceTracker.watchTransactions();
+
+    // initialize cron job here
+    new CronJob(
+        handleInput(config.schedule), // Everyday at 9:00 am
+        () => {
+            balanceTracker.watchBalances();
+        }
+    ).start();
 }
 
 const startTracker = async (name) => {
     // get default value and start the tracker
     const config = await getTrackerByName(name)
-
-    if (config.length == 1) {
-        let parsedConfig = config[config.length - 1];
-        let balanceTracker = new BalanceTracker(
-            parsedConfig.addresses,
-            parsedConfig.minimum,
-            new BalanceTrackerBot()
-        );
-        balanceTracker.subscribe("pendingTransactions");
-        balanceTracker.watchTransactions();
-
-        // initialize cron job here
+    if(config != null) {
+        await startListening(config);
     }
-    return false;
-}
+    return config;
+    
 
+}
 module.exports = {
     configTracker,
     startTracker
